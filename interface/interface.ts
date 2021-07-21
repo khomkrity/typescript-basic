605017e23911ab75f06193d43763f1008f3427bc
omekrit <khomkrit.y@gmail.com>
2021-07-21 22:47:06 +0700

    add interface as function type

--- interface/interface.ts
+++ interface/interface.ts
@@ -1,7 +1,20 @@
 // interface as type
+
 interface Employee {
   id: number;
   name: string;
 }
 
 let emp: Employee = { id: 1, name: 'Steve' };
+
+// interface as function type
+
+interface EmployeeProcess {
+  (id: number, name: string): void;
+}
+
+function printEmployee(id: number, name: string): void {
+  console.log('Employee: id = ' + id + ', value = ' + name);
+}
+
+printEmployee(emp.id, emp.name); //Output: updateKeyValue: key = 2, value = Steve
