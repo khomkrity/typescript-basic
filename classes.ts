122da581454e7d028eaf68f38e95dad7c88d5788
omekrit <khomkrit.y@gmail.com>
2021-07-24 22:44:24 +0700

    introduce inheritance

--- classes.ts
+++ classes.ts
@@ -2,7 +2,17 @@ class Vehicle {
   drive(): void {
     console.log("let's go!!!");
   }
+
+  honk(): void {
+    console.log('BEEP!');
+  }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
+class Car extends Vehicle {
+  drive(): void {
+    console.log('vroom');
+  }
+}
+
+const car = new Car();
+car.drive();
